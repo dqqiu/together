f6a00a42327088282b2115adaffe209d365da2a8
dqqiu <qiudequan624@163.com>
2016-10-16 18:32:47 +0800

    添加logback配置，项目依赖管理

---
+++ framework-boot-parent/framework-boot-core/src/main/java/org/spirit/framework/core/logger/TogetherPatternLayout.java
@@ -0,0 +1,18 @@
+package org.spirit.framework.core.logger;
+
+import ch.qos.logback.classic.PatternLayout;
+
+/**
+ * @Project       : framework-boot-core
+ * @Program Name  : org.spirit.framework.core.logger.TogetherPatternLayout.java
+ * @Description   : TODO qiudequan 日志布局模式重写
+ * @Author        : qiudequan
+ * @Creation Date : 2016年10月16日 下午5:50:07 
+ * @ModificationHistory  
+ * Who          When             What 
+ * ----------   -------------    -----------------------------------
+ * qiudequan     2016年10月16日        create
+ */
+public class TogetherPatternLayout extends PatternLayout {
+
+}
